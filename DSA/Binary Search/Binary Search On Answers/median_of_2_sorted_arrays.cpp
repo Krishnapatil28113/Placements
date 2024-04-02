c9163461ca6f8458a1f45fe5ca583ff883e6ff58
krishna <kp1406055@gmail.com>
2024-04-02 22:08:23 +0530

    Median Of 2 sorted arrays

--- DSA/Binary Search/Binary Search On Answers/median_of_2_sorted_arrays.cpp
+++ DSA/Binary Search/Binary Search On Answers/median_of_2_sorted_arrays.cpp
@@ -9,6 +9,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Brute Force (Merging two arrays and accessing the middle element)
 class Solution
 {
 public:
@@ -55,5 +56,112 @@ public:
         {
             return (double)arr[N / 2];
         }
+    }
+};
+
+// Better Approach (Traversing both the arrays once and finding the middle element based on the median indexes calculated prior to traversal)
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& arr1, vector<int>& arr2) {
+        int n1 = arr1.size();
+        int n2 = arr2.size();
+
+        int n = n1 + n2;
+
+        if (n % 2 != 0) {
+            int ele;
+            int index = (n - 1) / 2;
+
+            int count = 0;
+            int i = 0;
+            int j = 0;
+
+            while (i < n1 && j < n2) {
+                if (arr1[i] <= arr2[j]) {
+                    if (count == index) {
+                        return (double)arr1[i];
+                    } else {
+                        count++;
+                        i++;
+                    }
+                } else {
+                    if (count == index) {
+                        return (double)arr2[j];
+                    } else {
+                        count++;
+                        j++;
+                    }
+                }
+            }
+
+            for(;j<n2;j++) {
+                if(count == index) {
+                    return (double) arr2[j];
+                }
+                count++;
+            }
+            for(;i<n1;i++) {
+                if(count == index) {
+                    return (double) arr1[i];
+                }
+                count++;
+            }
+        } else {
+            int ele1 = -1;
+            int ele2 = -1;
+
+            int index1 = (n/2)-1;
+            int index2 = n/2;
+
+            int count = 0;
+            int i = 0;
+            int j = 0;
+
+            while (i < n1 && j < n2) {
+                if(arr1[i] <= arr2[j]) {
+                    if(count == index1) {
+                        ele1 = arr1[i];
+                    }
+
+                    if(count == index2) {
+                        ele2 = arr1[i];
+                    }
+                    count++;
+                    i++;
+                }
+                else {
+                    if(count == index1) {
+                        ele1 = arr2[j];
+                    }
+
+                    if(count == index2) {
+                        ele2 = arr2[j];
+                    }
+                    count++;
+                    j++;
+                }
+            }
+
+            for(;j<n2;j++) {
+                if(count == index1) {
+                    ele1 = arr2[j];
+                }
+                if(count == index2) {
+                    ele2 = arr2[j];
+                }
+                count++;
+            }
+            for(;i<n1;i++) {
+                if(count == index1) {
+                    ele1 = arr1[i];
+                }
+                if(count == index2) {
+                    ele2 = arr1[i];
+                }
+                count++;
+            }
+            return (double) (ele1 + ele2)/2.0;
+        }
+        return 0.0;
     }
 };
