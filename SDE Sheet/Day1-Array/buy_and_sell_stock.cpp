edbde6263372b5369eab1f17829179f624582516
krishna <kp1406055@gmail.com>
2024-06-01 00:12:09 +0530

    How to think - Remaining

---
+++ SDE Sheet/Day1-Array/buy_and_sell_stock.cpp
@@ -0,0 +1,28 @@
+/*
+# Problem Statement: To find the maximum profit that can be achieved by buying and selling stocks
+---------------------------------------------------------------
+# How to think: 
+---------------------------------------------------------------
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+        int n = arr.size();
+        int maxProfit = 0;
+
+        int minimum = arr[0];
+        int cost = 0;
+        for(int i=1;i<n;i++) {
+            cost = arr[i] - minimum;
+            maxProfit = max(maxProfit, cost);
+
+            minimum = min(minimum, arr[i]);
+        }
+        
+        return maxProfit;
+    }
+};
