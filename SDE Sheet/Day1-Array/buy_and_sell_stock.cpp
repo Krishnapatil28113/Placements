27fba22c2640f508428780a878f439fb521f6079
krishna <kp1406055@gmail.com>
2024-06-01 14:08:22 +0530

    Rotate Matrix by 90 Degrees

--- SDE Sheet/Day1-Array/buy_and_sell_stock.cpp
+++ SDE Sheet/Day1-Array/buy_and_sell_stock.cpp
@@ -1,28 +1,31 @@
 /*
 # Problem Statement: To find the maximum profit that can be achieved by buying and selling stocks
 ---------------------------------------------------------------
-# How to think: 
+# How to think:
 ---------------------------------------------------------------
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int maxProfit(vector<int>& arr) {
+    int maxProfit(vector<int> &arr)
+    {
         int n = arr.size();
         int maxProfit = 0;
 
         int minimum = arr[0];
         int cost = 0;
-        for(int i=1;i<n;i++) {
+        for (int i = 1; i < n; i++)
+        {
             cost = arr[i] - minimum;
             maxProfit = max(maxProfit, cost);
 
             minimum = min(minimum, arr[i]);
         }
-        
+
         return maxProfit;
     }
 };
